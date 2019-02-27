6eb59d24d2cd5c7b447832e2ba35aabd8b0090a5
Szymon Frankowski <sz.frankyy@gmail.com>
2019-02-27 16:22:22 +0100

    Sets support

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 function deepEqual(a, b) {
-	if (Array.isArray(a)) {
-		let eq = true;
+  let eq = true;
+  if (Array.isArray(a)) {
+    if (a.length === 0) {
+      if (b.length === 0) {
+        return true;
+      }
+      return false;
+    }
 
-		return a.length === b.length && ((a.length === 1 && a[0] === b[0]) || a.reduce((p, n, i) => {
-			return deepEqual(n, b[i]) && deepEqual(a[0],b[0]);
-		}));
-	};
+    return (
+      true ===
+      (typeof a === typeof b &&
+        a.length === b.length &&
+        ((a.length === 1 && a[0] === b[0]) ||
+          a.reduce((p, n, i) => {
+            return eq && deepEqual(n, b[i]) && deepEqual(a[0], b[0]);
+          })))
+    );
+  }
 
-	if (typeof a === 'object') {
-		let eq = true;
+  if (a instanceof Set) {
+    return (
+      b instanceof Set &&
+      a.size === b.size &&
+      (() => {
+        let eq2 = false;
 
-		for (let prop in a) {
-			eq = eq && deepEqual(a[prop], b[prop]);
-		};
+        for (let x in a) {
+          eq = eq && eq2;
+          for (let y in b) {
+            eq2 = eq2 || deepEqual(x, y);
+          }
+        }
+        return eq;
+      })()
+    );
+  }
 
-		for (let prop in b) {
-			eq = eq && !(typeof b[prop] !== 'undefined' && typeof a[prop] === 'undefined');
-		}
-		return eq;
-	}
-	return a === b;
+  if (typeof a === 'object') {
+    for (let prop in a) {
+      eq = eq && deepEqual(a[prop], b[prop]);
+    }
 
-};
-
-
-exports.helpers = {
-	deepEqual
+    for (let prop in b) {
+      eq =
+        eq &&
+        !(typeof b[prop] !== 'undefined' && typeof a[prop] === 'undefined');
+    }
+    return eq;
+  }
+  return a === b;
 }
+
+exports.deepEqual = deepEqual;
